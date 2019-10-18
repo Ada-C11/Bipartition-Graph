8721d3786cdeb3e722120e896d72a175948cf804
sjscotton <sj.scotton@gmail.com>
2019-10-17 19:06:52 -0700

    All tests passing

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,30 @@
+def visit_node(stack, nodes, key, group)
+  node = { :key => key, :group => group }
+  nodes[key] = node
+  stack.append(node)
+end
 
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  return true if dislikes == []
+  stack = []
+  nodes = Array.new(dislikes.length)
+
+  dislikes.length.times do |curr|
+    visit_node(stack, nodes, curr, 0) if !nodes[curr]
+    until stack.empty?
+      curr_node = stack.pop()
+      curr_dislikes = dislikes[curr_node[:key]]
+      curr_group = curr_node[:group]
+
+      curr_dislikes.each do |node|
+        if nodes[node]
+          return false if nodes[node][:group] == curr_group
+        else
+          group = (curr_group + 1) % 2
+          visit_node(stack, nodes, node, group)
+        end
+      end
+    end
+  end
+  return true
 end
