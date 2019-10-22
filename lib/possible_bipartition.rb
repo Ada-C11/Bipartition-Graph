404cd527e1e548a0dc9599f2d634cc88b1e0f9d3
Amy Wyatt <amy.wyatt12@outlook.com>
2019-10-22 00:35:32 -0700

    passing 5 tests

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,42 @@
+require "set"
 
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  if dislikes.empty?
+    return true
+  end
+
+  dogz = Set.new()
+  doggos = Set.new()
+
+  dislikes.each_with_index { |dislike, i|
+    if !dogz.include?(i) && !doggos.include?(i)
+      dogz.add(i)
+
+      dislike.each { |dog|
+        if dogz.include?(dog)
+          return false
+        else
+          doggos.add(dog)
+        end
+      }
+    elsif dogz.include?(i)
+      dislike.each { |dog|
+        if dogz.include?(dog)
+          return false
+        else
+          doggos.add(dog)
+        end
+      }
+    elsif doggos.include?(i)
+      dislike.each { |dog|
+        if doggos.include?(dog)
+          return false
+        else
+          dogz.add(dog)
+        end
+      }
+    end
+  }
+
+  return true
 end
