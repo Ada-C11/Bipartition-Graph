4ec465a75e882bbcb1e8498d84fcecbab7884353
Amy Wyatt <amy.wyatt12@outlook.com>
2019-10-22 01:49:18 -0700

    add tests passing

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -8,17 +8,21 @@ def possible_bipartition(dislikes)
   dogz = Set.new()
   doggos = Set.new()
 
-  dislikes.each_with_index { |dislike, i|
-    if !dogz.include?(i) && !doggos.include?(i)
-      dogz.add(i)
+  i = 0
+  return bipartition_helper(i, dislikes, dogz, doggos)
+end
 
-      dislike.each { |dog|
-        if dogz.include?(dog)
-          return false
-        else
-          doggos.add(dog)
-        end
-      }
+def bipartition_helper(i, dislikes, dogz, doggos)
+  while i < dislikes.length
+    dislike = dislikes[i]
+    if !dogz.include?(i) && !doggos.include?(i)
+      dogz1 = dogz.clone()
+      doggos1 = doggos.clone()
+
+      dogz1.add(i)
+      doggos.add(i)
+
+      return bipartition_helper(i, dislikes, dogz1, doggos1) || bipartition_helper(i, dislikes, dogz, doggos)
     elsif dogz.include?(i)
       dislike.each { |dog|
         if dogz.include?(dog)
@@ -36,7 +40,8 @@ def possible_bipartition(dislikes)
         end
       }
     end
-  }
 
+    i += 1
+  end
   return true
 end
